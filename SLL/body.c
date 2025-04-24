36c8a222460b68a3944fb938cd459d712629f30d
Faliq Azzaki <faliqazzaki@gmail.com>
2025-04-24 22:26:03 +0700

    first commit tugas

---
+++ SLL/body.c
@@ -0,0 +1,76 @@
+#include "head.h"
+#include <malloc.h>
+#include <string.h>
+#define Nil NULL
+
+void alokasiArray(arrayKota *P){
+    scanf("%d", &(*P).length);
+    (*P).kota = malloc(sizeof(kota) * (*P).length);
+}
+
+address alokasi(char *nama){
+    address P = (address)malloc(sizeof(warga));
+    if(P != Nil){
+        strcpy(P->nm, nama);
+        P->q = Nil;
+    }else{
+        printf("alokasi gagal !!");
+    }
+    return P;
+}
+
+void addWarga(arrayKota *P, char *nama, char *domisili){
+    address warga = alokasi(nama);
+    int i = 0;
+    address temp;
+    if(warga != Nil){
+        while(strcmp((*P).kota[i].kt, domisili) != 0){
+            if(strcmp((*P).kota[i].kt, domisili) == 0){
+                break;
+            }
+            i++;
+        }
+        // terjadi jika tidak ada sebuah kota
+        if(i >= (*P).length){
+            printf("tidak ada kota !!!");
+        }else{
+            // terjadi jika terdapat sebuah kota 
+            temp = (*P).kota[i].p;
+            if(temp != Nil){
+                while(temp->q != Nil){
+                    temp = temp->q;
+                }   
+                temp->q = warga;
+            }else{
+                (*P).kota[i].p = warga;
+            }
+        }
+    }else{
+        printf("alokasi gagal !!");
+    }
+}
+
+void printMasyarakat(arrayKota P){
+    int i;
+    address temp;
+    if(P.kota != Nil){
+        i = 0;
+        while(i < P.length){
+            printf("Kota %d : %s", i+1, P.kota[i].kt);
+            printf("\nWarga : ");
+            temp = P.kota[i].p;
+            if(temp != Nil){
+                while(temp != Nil){
+                    printf("%s , ", temp->nm);
+                    temp = temp->q;
+                }
+            }else{
+                printf("(Nil)");
+            }
+            printf("\n");
+            i++;
+        }
+    }else{
+        printf("tidak ada kota dan warga");
+    }
+}
---
+++ SLL/head.h
@@ -0,0 +1,28 @@
+#ifndef HEAD_H
+#define HEAD_H
+#define MAX 100
+
+typedef struct warga* address;
+// membuat node untuk warga
+typedef struct warga{
+    char nm[MAX];
+    address q;
+}warga;
+
+// membuat node untuk kota
+typedef struct kota{
+    char kt[MAX];
+    address p;
+}kota;
+
+typedef struct arrayKota{
+    int length;
+    kota *kota;
+}arrayKota;
+
+void alokasiArray(arrayKota *P);
+address alokasi(char *nama);
+void addWarga(arrayKota *P, char *nama, char *domisili);
+void printMasyarakat(arrayKota P);
+
+#endif
